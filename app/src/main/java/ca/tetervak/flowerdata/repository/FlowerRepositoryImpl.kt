f7587acacd6412af65e2b4d9c65a1ff0602fb443
Alex Tetervak <tetervak@sheridancollege.ca>
2021-02-22 14:58:31 -0500

    small code cleanups

--- app/src/main/java/ca/tetervak/flowerdata/repository/FlowerRepositoryImpl.kt
+++ app/src/main/java/ca/tetervak/flowerdata/repository/FlowerRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.liveData
 import ca.tetervak.flowerdata.domain.Flower
+import ca.tetervak.flowerdata.network.CatalogJson
 import ca.tetervak.flowerdata.network.FlowerDataApi
 import ca.tetervak.flowerdata.network.FlowerJson
 import ca.tetervak.flowerdata.network.IMAGE_FOLDER_URL
@@ -12,8 +13,8 @@ import javax.inject.Inject
 class FlowerRepositoryImpl @Inject constructor(): FlowerRepository {
 
     private var flowerListData: LiveData<List<Flower>> = liveData {
-        val catalog = FlowerDataApi.retrofitService.getCatalog()
-        val flowers = catalog.flowers.mapIndexed { index, flowerJson ->
+        val catalog: CatalogJson = FlowerDataApi.retrofitService.getCatalog()
+        val flowers: List<Flower> = catalog.flowers.mapIndexed { index, flowerJson ->
             flowerJson.asFlower(index)
         }
         emit(flowers)
