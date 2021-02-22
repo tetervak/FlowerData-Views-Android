218575d898b878cf6c678eeb92cc1118dccf37d2
Alex Tetervak <tetervak@sheridancollege.ca>
2021-02-21 21:31:54 -0500

    fixed the deprecated scope annotation

--- app/src/main/java/ca/tetervak/flowerdata/repository/RepositoryModule.kt
+++ app/src/main/java/ca/tetervak/flowerdata/repository/RepositoryModule.kt
@@ -3,10 +3,10 @@ package ca.tetervak.flowerdata.repository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
     @Binds
