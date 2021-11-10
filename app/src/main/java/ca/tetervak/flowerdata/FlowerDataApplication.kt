1e0689fe3a5dab0e22d41025ce6aef2af2fb670b
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-11-10 18:15:34 -0500

    used the work manager

--- app/src/main/java/ca/tetervak/flowerdata/FlowerDataApplication.kt
+++ app/src/main/java/ca/tetervak/flowerdata/FlowerDataApplication.kt
@@ -1,7 +1,45 @@
 package ca.tetervak.flowerdata
 
 import android.app.Application
+import android.os.Build
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.*
+import ca.tetervak.flowerdata.workmanager.RefreshWorker
 import dagger.hilt.android.HiltAndroidApp
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 @HiltAndroidApp
-class FlowerDataApplication: Application()
+class FlowerDataApplication: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresCharging(false)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            RefreshWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+
+    }
+
+
+}
